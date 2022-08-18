682acc305134915af6ded0cb791233a7ac2f22de
Artem <artem138927@gmail.com>
2022-08-18 12:17:09 +0200

    Version 0.2

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
 // By Chernysh Artem.
-
-
-// Function that chooses randomly rock, paper or scissors.
+// Function that chooses randomly rock, paper or scissors, and returns it.
 function getComputerChoice() {
-    const computerOptions = ['rock', 'paper', 'scissors'];
-    let computerChoice;
-
-    const random = Math.floor(Math.random() * computerOptions.length) + 1;
-
-    if (random === 1) {
-        return computerChoice = computerOptions[0];
-    } else if (random === 2) {
-        return computerChoice = computerOptions[1];
-    } else if (random === 3) {
-        return computerChoice = computerOptions[2];
-    } else {
-        return console.log('getComputerChoice error: Unknown computerChoice')
-    }
+    let options = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random() * options.length);
+    let computerChoice = options[random];
+    return computerChoice;
 }
 
+let result;
 
 // Function that plays single round.
 function playRound (playerSelection, computerSelection) {
     // variants of playRound:
     //if player chose rock:
     if (playerSelection === 'rock' && computerSelection === 'rock') {
-        console.log("It's a tie!");
+        return result = "It's a tie.";
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('Player lost.');
+        return result = "Player lost.";
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('Player won!')
+        return result = "Player won!";
     } // if player chose paper:
     else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        console.log("It's a tie!")
+        return result = "It's a tie.";
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('Player lost.')
+        return result = "Player lost.";
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('Player won!')
+        return result = "Player won!";
     } // if player chose scissors:
     else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log("It's a tie!")
+        return result = "It's a tie.";
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('Player lost.')
+        return result = "Player lost.";
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('Player won!')
+        return result = "Player won!";
     } //else
     else {
-        console.log('Error: something went wrong in "playRound()".')
+        return result = "Something went wrong in playRound().";
     }
 }
 
@@ -57,16 +46,16 @@ function getInputWeapon() {
     return inputWeapon = prompt('Please enter a weapon to play: ', '');
 }
 
-const playerSelection = getInputWeapon();
-const computerSelection = getComputerChoice();
-
 // Function that plays "Rock paper scissors".
 function game() {
     getInputWeapon();
+    let playerSelection = getInputWeapon();
+    getComputerChoice();
+    let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    console.log(result);
 }
 
-// Plays "game()" 5 times.
 for (let i = 0; i < 5; i++) {
     game();
 }
